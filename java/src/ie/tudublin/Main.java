cc27658354d35897e504b6bebd7d9c44e446e1da
Jon Kehoe <jonpkehoe@outlook.com>
2023-04-29 15:14:29 +0100

    commit6

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -8,15 +8,19 @@ import C21336753.jon;
 public class Main
 {	
 
-	public void startUI()
+
+
+	public void jon()
 	{
 		String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new MyVisual());		
+        processing.core.PApplet.runSketch( a, new jon());		
 	}
+
+
 
 	public static void main(String[] args)
 	{
 		Main main = new Main();
-		main.startUI();			
+		main.jon();			
 	}
 }
