ce7489f6b6e806f0d498311b98eb8bf81c8a7d75
Jon Kehoe <jonpkehoe@outlook.com>
2023-04-29 17:09:06 +0100

    andrew code edited

--- java/src/C21336753/david.java
+++ java/src/C21336753/david.java
@@ -1,53 +1,53 @@
-package C21336753;
+// package C21336753;
 
-import ddf.minim.*;
-import processing.core.*;
-import ddf.minim.AudioPlayer;
-import ddf.minim.analysis.*;
-import processing.core.PApplet;
+// import ddf.minim.*;
+// import processing.core.*;
+// import ddf.minim.AudioPlayer;
+// import ddf.minim.analysis.*;
+// import processing.core.PApplet;
 
-public class david extends PApplet {
+// public class david extends PApplet {
 
-    Minim minim;
-    AudioPlayer audioPlayer;
-    FFT fft;
-    float[] spectrum;
+//     Minim minim;
+//     AudioPlayer audioPlayer;
+//     FFT fft;
+//     float[] spectrum;
 
-    float angleX = 0;
-    float angleY = 0;
-    float angleZ = 0;
+//     float angleX = 0;
+//     float angleY = 0;
+//     float angleZ = 0;
 
-    public void setup() {
-        size(640, 480, P3D);
+//     public void setup() {
+//         size(640, 480, P3D);
 
-        minim = new Minim(this);
-        audioPlayer = minim.loadFile("song.mp3");
-        audioPlayer.loop();
-    }
+//         minim = new Minim(this);
+//         audioPlayer = minim.loadFile("song.mp3");
+//         audioPlayer.loop();
+//     }
 
-    public void draw() {
-        background(255);
-        lights();
-        stroke(0);
-        noFill();
+//     public void draw() {
+//         background(255);
+//         lights();
+//         stroke(0);
+//         noFill();
 
-        fft.analyze();
-        spectrum = fft.spectrum();
+//         fft.analyze();
+//         spectrum = fft.spectrum();
 
-        float bass = spectrum[0];
-        float treble = spectrum[spectrum.length - 1];
+//         float bass = spectrum[0];
+//         float treble = spectrum[spectrum.length - 1];
 
-        angleX += map(bass, 0, 255, 0.01, 0.5);
-        angleY += map(treble, 0, 255, 0.01, 0.5);
-        angleZ += 0.01;
+//         angleX += map(bass, 0, 255, 0.01, 0.5);
+//         angleY += map(treble, 0, 255, 0.01, 0.5);
+//         angleZ += 0.01;
 
-        pushMatrix();
-        translate(width / 2, height / 2, 0);
-        rotateX(angleX);
-        rotateY(angleY);
-        rotateZ(angleZ);
-        box(100);
-        popMatrix();
-    }
+//         pushMatrix();
+//         translate(width / 2, height / 2, 0);
+//         rotateX(angleX);
+//         rotateY(angleY);
+//         rotateZ(angleZ);
+//         box(100);
+//         popMatrix();
+//     }
 
-}
+// }
