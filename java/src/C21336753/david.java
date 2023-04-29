f34fb3ef904eb1296980f0bbeae3158daf5547ae
andrewevnas <you@example.com>
2023-04-30 00:05:10 +0100

    davidfix

--- java/src/C21336753/david.java
+++ java/src/C21336753/david.java
@@ -9,27 +9,27 @@ public class david extends PApplet{
 
     float angle;
 
-void setup()
-{
-    size(960, 1080);
-    surface.setLocation(957,0);
-    rectMode(CENTER);
-    stroke (0,0,0);
-    strokeWeight(25);
-}
-
-void draw()
-{
-  background(54,69,79);
-  translate(width/2, height/2);
-  for(int i=0; i<100; i++)
+public void setup()
   {
-    fill(i*16, 164-i*50,255-i*10);
-    scale(0.95);
-  
-  rotate(radians(angle));
-  rect(0, 0, 600, 600);
+      size(960, 1080);
+      surface.setLocation(957,0);
+      rectMode(CENTER);
+      stroke (0,0,0);
+      strokeWeight(25);
   }
-  angle+=0.1;
-}
+
+  public void draw()
+  {
+    background(54,69,79);
+    translate(width/2, height/2);
+    for(int i=0; i<100; i++)
+    {
+      fill(i*16, 164-i*50,255-i*10);
+      scale(0.95);
+    
+    rotate(radians(angle));
+    rect(0, 0, 600, 600);
+    }
+    angle+=0.1;
+  }
 }
